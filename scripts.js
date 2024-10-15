77682eff5740c5cea2ac55c22d7c55ff7e0ccd02
AlexRiley99 <alexriley1099@gmail.com>
2024-10-15 13:08:43 -0400

    linked each page to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /*Search Bar Functionality*/
 function handleSearch(event){
     event.preventDefault(); //prevent default form submission
-    const searchQuery = document.getElementById('searchInput').value; //get input value
+    const searchQuery = document.getElementById('searchQuery').value; //get input value
     if(searchQuery){
     //redirect to search results page with query
     window.location.href=`/search?query=${encodeUIRComponent(searchQuery)}`;
