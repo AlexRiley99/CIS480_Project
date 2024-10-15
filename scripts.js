46089363e91426f9ac3f8e15b18cbd6fcfaca552
AlexRiley99 <alexriley1099@gmail.com>
2024-10-15 15:13:49 -0400

    Modified scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,25 @@
 /*Search Bar Functionality*/
-function handleSearch(event){
-    event.preventDefault(); //prevent default form submission
-    const searchQuery = document.getElementById('searchInput').value; //get input value
-    if(searchQuery){
-    //redirect to search results page with query
-    window.location.href=`/search?query=${encodeURIComponent(searchQuery)}`;
+function handleSearch(){
+    const searchQuery = document.getElementById('searchQuery').value.toLowerCase(); //get input value
+    
+    //redirect based on keywords
+    if(searchQuery.includes("class") || searchQuery.includes("boxing") ||
+        searchQuery.includes("yoga") || searchQuery.includes("pilates") ||
+        searchQuery.includes("hiit") || searchQuery.includes("water") || 
+        searchQuery.includes("dance") || searchQuery.includes("aerobics") ||
+        searchQuery.includes("zumba") || searchQuery.includes("jazzercise")
+    ){
+        window.location.href = "/classes"; //redirect to classes
     }
+    else if(searchQuery.includes("hours") || searchQuery.includes("childcare") ||
+        searchQuery.includes("operation") || searchQuery.includes("contact") 
+        ){
+        window.location.href = "/landing"; //redirect to landing page
+    }
+    else if(searchQuery.includes("log") || searchQuery.includes("account") ||
+    searchQuery.includes("create")
+        ){
+        window.location.href = "/login"; //redirect to login page
+    }
+
 }
