689a4b961bb1537faafe21a683f3dc04bf50298b
AlexRiley99 <alexriley1099@gmail.com>
2024-10-15 13:00:58 -0400

    Added an id to the search bar so JavaScript can handle search queries

---
+++ scripts.js
@@ -0,0 +1,9 @@
+/*Search Bar Functionality*/
+function handleSearch(event){
+    event.preventDefault(); //prevent default form submission
+    const searchQuery = document.getElementById('searchInput').value; //get input value
+    if(searchQuery){
+    //redirect to search results page with query
+    window.location.href=`/search?query=${encodeUIRComponent(searchQuery)}`;
+    }
+}
