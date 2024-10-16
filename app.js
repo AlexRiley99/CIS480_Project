e246c826733272e9accf83a94eff9e8c5936b7dd
AlexRiley99 <alexriley1099@gmail.com>
2024-10-16 17:45:36 -0400

    Changed "public" to "docs" to try and get GitHub Pages to work

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const db = new sqlite3.Database('FlexAndFlowDB.db');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Serve static files from the 'public' directory
-app.use(express.static('public'));
+//Serve static files from the 'docs' directory
+app.use(express.static('docs'));
 
 // Routes for each page
      //Default Page
@@ -22,15 +22,15 @@ app.use(express.static('public'));
             });
     //Landing Page
     app.get('/landing', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'LandingPage', 'index.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'LandingPage', 'index.html'));
     });
     //About Page
     app.get('/about', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'AboutPage', 'About.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'AboutPage', 'About.html'));
     });
     //Login Page
     app.get('/login', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'LoginPage', 'Login.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'LoginPage', 'Login.html'));
     });
 
         // Login endpoint
@@ -54,71 +54,71 @@ app.use(express.static('public'));
 
     //Sign Up Page
     app.get('/signup', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'SignUpPage', 'SignUp.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'SignUpPage', 'SignUp.html'));
     });
     //Plans Page
     app.get('/plans', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'PlansPage', 'Plans.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'PlansPage', 'Plans.html'));
     });
     //Classes Page
     app.get('/classes', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'ClassesPage', 'Classes.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'ClassesPage', 'Classes.html'));
     });
     //Class Sign Up Page
     app.get('/class_signup', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'ClassSignUp', 'ClassSignUp.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'ClassSignUp', 'ClassSignUp.html'));
     });
     //AccountInfoPage
     app.get('/accountinfo', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'AccountInfoPage', 'AccountInfo.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'AccountInfoPage', 'AccountInfo.html'));
     });
     //AccountPage
     app.get('/account', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'AccountPage', 'Account.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'AccountPage', 'Account.html'));
     });
     //ChangePlanPage
     app.get('/changeplan', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'ChangePlanPage', 'ChangePlan.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'ChangePlanPage', 'ChangePlan.html'));
     });
     //ChildProfilesPage
     app.get('/childprofiles', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'ChildProfilesPage', 'ChildProfiles.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'ChildProfilesPage', 'ChildProfiles.html'));
     });
     //ExampleProfile
     app.get('/exampleprofile', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'ExampleProfile', 'ExampleProfile.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'ExampleProfile', 'ExampleProfile.html'));
     });
     //JournalPage
     app.get('/journal', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'JournalPage', 'Journal.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'JournalPage', 'Journal.html'));
     });
     //ManageChildrenPage
     app.get('/managechildren', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'ManageChildrenPage', 'ManageChildren.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'ManageChildrenPage', 'ManageChildren.html'));
     });
     //ManagePlanPage
     app.get('/manageplan', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'ManagePlanPage', 'ManagePlan.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'ManagePlanPage', 'ManagePlan.html'));
     });
     //NewChildConsentPage
     app.get('/newchildconsent', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'NewChildConsentPage', 'NewChildConsentAgreement.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'NewChildConsentPage', 'NewChildConsentAgreement.html'));
     });
     //NewChildDisclosurePage
     app.get('/newchilddisclosure', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'NewChildDisclosurePage', 'NewChildDisclosureAgreement.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'NewChildDisclosurePage', 'NewChildDisclosureAgreement.html'));
     });
     //NewChildPage
     app.get('/newchild', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'NewChildPage', 'NewChild.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'NewChildPage', 'NewChild.html'));
     });
     //NewMemberConsentPage
     app.get('/newmemberconsent', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'NewMemberConsentPage', 'NewMemberConsentAgreement.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'NewMemberConsentPage', 'NewMemberConsentAgreement.html'));
     });
     //NewMemberDisclosure
     app.get('/newmemberdisclosure', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'NewMemberDisclosurePage', 'NewMemberDisclosureAgreement.html'));
+        res.sendFile(path.join(__dirname, 'docs', 'NewMemberDisclosurePage', 'NewMemberDisclosureAgreement.html'));
     });
 
 
