b14f77ee08859f35470dfa564a7981135f3b3798
AlexRiley99 <alexriley1099@gmail.com>
2024-10-26 03:45:49 -0400

    Added some temporary code to app.js and SignUp.html

--- Database/app.js
+++ Database/app.js
@@ -131,3 +131,17 @@ document.getElementById('signupForm').addEventListener('submit', async (e) => {
         document.getElementById('message').textContent = "Error uploading data.";
     }
 });
+
+//TEMPORARY CODE
+document.getElementById('addButton').addEventListener('click', () => {
+    addExistingDocuments("Members").catch(console.error);
+    addExistingDocuments("Children").catch(console.error);
+    addExistingDocuments("Plans").catch(console.error);
+    addExistingDocuments("Classes").catch(console.error);
+    addExistingDocuments("EmergencyContacts").catch(console.error);
+    addExistingDocuments("AuthorizedAdults").catch(console.error);
+    addExistingDocuments("ClassEnrollments").catch(console.error);
+    addExistingDocuments("JournalEntries").catch(console.error);
+    addExistingDocuments("Testing").catch(console.error);
+    addExistingDocuments("LoginInfo").catch(console.error);
+})
