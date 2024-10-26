ba0b7bf1b4e2bf3011c731812829d95b01beb58b
AlexRiley99 <alexriley1099@gmail.com>
2024-10-26 04:51:27 -0400

    Added new GitHub Actions Workflow and began hosting with Firebase

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'CIS480_Project',
+  location: 'us-central1'
+};
+
