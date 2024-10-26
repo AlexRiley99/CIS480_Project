1df69a913e10e878788c252b8a7ad80076177f80
AlexRiley99 <alexriley1099@gmail.com>
2024-10-26 19:03:26 -0400

    troubleshooting

--- Database/serverSide.js
+++ Database/serverSide.js
@@ -1,7 +1,2 @@
 /****************SERVER SIDE CODE*******************/
-const functions = require('firebase-functions');
 
-exports.myFunction = functions.https.onRequest((req, res) => {
-    //server-side code here
-    res.send("Hello from Cloud Functions!");
-});
